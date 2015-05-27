77c37b740890c740e4dc845f5dcbfc2f22dd84d1
AnanasowyOcelot <lukasz.woroniecki@gmail.com>
2015-05-28 00:36:14 +0200

    PrintAllPairsInSortedArrayThatSumsToM

--- src/companies/forgotten/PrintAllPairsInSortedArrayThatSumsToM.java
+++ src/companies/forgotten/PrintAllPairsInSortedArrayThatSumsToM.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public class PrintAllPairsInSortedArrayThatSumsToM {
 
-    public void count(ArrayList<Integer> sortedArray, int number){
+    public void count(ArrayList<Integer> sortedArray, int number) {
         int left = 0;
         int right = sortedArray.size() - 1;
-        while(left < right){
+        while (left < right) {
             int sum = sortedArray.get(left) + sortedArray.get(right);
-            while(sum < number){
+            if (sum < number) {
                 left++;
-                sum = sortedArray.get(left) + sortedArray.get(right);
             }
-            while (sum > number){
+            if (sum > number) {
                 right--;
-                sum = sortedArray.get(left) + sortedArray.get(right);
             }
-            System.out.println(left + "-" + right);
-            left++;
-            right++;
+            if (sum == number) {
+                System.out.println(left + "-" + right);
+                left++;
+                right++;
+            }
         }
     }
 
